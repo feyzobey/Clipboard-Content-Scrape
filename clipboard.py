3c7789b03f82a9d4328bc2d8a588124436242d32
Feyzullah Asıllıoğlu <feyzullahasillioglu@gmail.com>
2022-06-11 23:37:21 +0300

    added path feature

--- clipboard.py
+++ clipboard.py
@@ -2,7 +2,8 @@ from tkinter import Tk
 import git
 
 try:
-    git.Git().clone(Tk().clipboard_get())
+    # in which directory you want to clone, type its path into Git() method   
+    git.Git("../").clone(Tk().clipboard_get())
 except:
     print("Error: Could not clone repository")
 finally:
