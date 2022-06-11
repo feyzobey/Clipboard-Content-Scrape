e687e049ab3d14d48db57cd3b66a722106a9f1d7
Feyzullah Asıllıoğlu <feyzullahasillioglu@gmail.com>
2022-06-11 23:27:31 +0300

    updated clipboard.py

--- clipboard.py
+++ clipboard.py
@@ -1,4 +1,9 @@
 from tkinter import Tk
 import git
-git.Git().clone(Tk().clipboard_get())
-print("Clipboard: " + Tk().clipboard_get()) # Prints the clipboard contents
+
+try:
+    git.Git().clone(Tk().clipboard_get())
+except:
+    print("Error: Could not clone repository")
+finally:
+    print("Clipboard: " + Tk().clipboard_get())
